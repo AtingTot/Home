cf914b262d80169a61a38f1209f32a2063933836
kexiaofeng <kexiaofeng.uh@haier.com>
2021-03-30 11:26:53 +0800

    提交TextField中后面跟着按钮实现方式

--- home/lib/pages/sms_login_page.dart
+++ home/lib/pages/sms_login_page.dart
@@ -75,7 +75,6 @@ class _SMSLoginPageState extends State<SMSLoginPage> {
         title: Text("欢迎登录智慧家"),
       ),
       body: Container(
-        // height: 360,
         padding: EdgeInsets.only(left:20, right: 20, top: 60),
         child: Column(
           children: <Widget> [
@@ -94,7 +93,6 @@ class _SMSLoginPageState extends State<SMSLoginPage> {
                 // hintText: '请输入11位手机号',
                 labelText: '请输入11位手机号',
                 icon: Icon(Icons.phone),
-                // contentPadding: EdgeInsetsGeometry.infinity,
                 suffixIcon: !_canClearPhone ? null : IconButton(
                   icon: Icon(Icons.cancel),
                   onPressed: () {
@@ -106,29 +104,25 @@ class _SMSLoginPageState extends State<SMSLoginPage> {
                 )
               ),
             ),
-            Stack(
-              alignment: const Alignment(1.0, 0.0),
-              children: [
-                TextField(
-                  focusNode: _codeFocus,
-                  keyboardType: TextInputType.number,
-                  controller: _codeController,
-                  onChanged: (text) {
-                    setState(() {
-                      _canClearCode = text.isNotEmpty;
-                    });
-                  },
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    // hintText: '请输入验证码',
-                    labelText: '请输入验证码',
-                    icon: Icon(Icons.sms),
-                    // prefix: null,
-                    // suffix: null,
-                    // prefixIcon: Icon(Icons.sms),
-                    // border: null,
-                    suffixIcon: !_canClearCode ? null : IconButton(
-                      padding: EdgeInsets.only(right: 120),
+            TextField(
+              focusNode: _codeFocus,
+              keyboardType: TextInputType.number,
+              controller: _codeController,
+              onChanged: (text) {
+                setState(() {
+                  _canClearCode = text.isNotEmpty;
+                });
+              },
+              obscureText: false,
+              decoration: InputDecoration(
+                // hintText: '请输入验证码',
+                labelText: '请输入验证码',
+                icon: Icon(Icons.sms),
+                suffixIconConstraints:  BoxConstraints(maxHeight: 32),
+                suffixIcon: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    !_canClearCode ? Text("") : IconButton(
                       icon: Icon(Icons.cancel),
                       onPressed: () {
                         _codeController.clear();
@@ -136,44 +130,46 @@ class _SMSLoginPageState extends State<SMSLoginPage> {
                           _canClearCode = false;
                         });
                       },
-                    )
-                  ),
-                ),
-                ElevatedButton(
-                  child: Text(codeBtnTip, style: TextStyle(fontSize: 12,),),
-                  onPressed: () {
-                    Fluttertoast.showToast(msg: '短信验证码已发送，请注意查收');
-                    getVerificationCode();
-                    print("begin get code");
-                    if (_countdownTimer != null) {
-                      return;
-                    }
-                    _isClickGetCode = true;
-                    _seconds = 60;
-                    setState(() {
-                      codeBtnTip = "$_seconds秒";
-                      _seconds--;
-                    });
-                    _countdownTimer = Timer.periodic(Duration(seconds: 1), (timer) {
-                      setState(() {
-                        codeBtnTip = "$_seconds秒";
-                        if (_seconds > 0) {
-                          _seconds--;
-                        } else {
-                          _countdownTimer.cancel();
-                          _countdownTimer = null;
-                          codeBtnTip = "重新获取";
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Fluttertoast.showToast(msg: '短信验证码已发送，请注意查收');
+                        getVerificationCode();
+                        print("begin get code");
+                        if (_countdownTimer != null) {
+                          return;
                         }
-                      });
-                    });
-                  },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(StadiumBorder()),
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                  ),
+                        _isClickGetCode = true;
+                        _seconds = 60;
+                        setState(() {
+                          codeBtnTip = "$_seconds秒";
+                          _seconds--;
+                        });
+                        _countdownTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+                          setState(() {
+                            codeBtnTip = "$_seconds秒";
+                            if (_seconds > 0) {
+                              _seconds--;
+                            } else {
+                              _countdownTimer.cancel();
+                              _countdownTimer = null;
+                              codeBtnTip = "重新获取";
+                            }
+                          });
+                        });
+                      },
+                      child: Text(codeBtnTip, style: TextStyle(fontSize: 12,),),
+                      style: ButtonStyle(
+                        // padding: MaterialStateProperty.all(EdgeInsets.only(top:8,bottom:8)),
+                        // minimumSize: MaterialStateProperty.all(Size(120, 24)),
+                        shape: MaterialStateProperty.all(StadiumBorder()),
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        foregroundColor: MaterialStateProperty.all(Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
             Padding(padding: EdgeInsets.only(top: 20)),
             ElevatedButton(
@@ -181,14 +177,9 @@ class _SMSLoginPageState extends State<SMSLoginPage> {
                 print("我点击了");
               },
               child: Text("注册/登录", style: TextStyle(fontSize: 18,),),
-              // //通过控制Text的边距来控制控件的高度
-              // child: Padding(padding: EdgeInsets.only(top: 12.0, bottom: 12.0),
-              //   child: Text("注册/登录", style: TextStyle(fontSize: 18,),),
-              // ),
               style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(
-                  Size(MediaQuery.of(context).size.width, 48)
-                ),
+                // minimumSize: MaterialStateProperty.all(Size(MainAxisSize.min, 48)),
+                minimumSize: MaterialStateProperty.all(Size(MediaQuery.of(context).size.width, 48)),
                 shape: MaterialStateProperty.all(StadiumBorder()),
                 backgroundColor: MaterialStateProperty.all(Colors.blue),
                 foregroundColor: MaterialStateProperty.all(Colors.white),
@@ -225,7 +216,7 @@ class _SMSLoginPageState extends State<SMSLoginPage> {
             //     ),
             //     onPressed: () => print("width = ${MediaQuery.of(context).size.width}"),
             //     style: ButtonStyle(
-            //       minimumSize: MaterialStateProperty.all(Size(MediaQuery.of(context).size.width,60)),
+            //       minimumSize: MaterialStateProperty.all(Size(MediaQuery.of(context).size.width,48)),
             //       // tapTargetSize: ,
             //       // backgroundColor: MaterialStateProperty.all(Colors.black),
             //       // padding: MaterialStateProperty.all(EdgeInsets.only(left: MediaQuery.of(context).size.width/2, right: MediaQuery.of(context).size.width/2)),
